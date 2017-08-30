cf5d52b9bc1b831086294ac10b615d63110ee144
guxin <525205430@qq.com>
2017-08-30 11:59:58 +0800

    使用JSONObject解析物品Item 开始制作UI

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -1,7 +1,5 @@
 ﻿using UnityEngine;
-using System.Collections;
 using System.Collections.Generic;
-using System;
 using Newtonsoft.Json;
 
 public class InventoryManager : MonoBehaviour {
@@ -24,7 +22,7 @@ public class InventoryManager : MonoBehaviour {
         ParseItemJson();
     }
 
-    private List<Item> itemList;
+    private List<Item> itemList = new List<Item>();
     /// <summary>
     /// 解析物品Json
     /// </summary>
@@ -33,8 +31,45 @@ public class InventoryManager : MonoBehaviour {
         TextAsset textAsset = Resources.Load<TextAsset>("Items");
         string itemsJson = textAsset.text;
 
-        List<Item> itemList = JsonConvert.DeserializeObject<List<Item>>(itemsJson);
-       
+        //List<Item> itemList = JsonConvert.DeserializeObject<List<Item>>(itemsJson);
+        JSONObject j = new JSONObject(itemsJson);
+
+        foreach (JSONObject temp in j.list)
+        {
+            int id = (int)temp["id"].n;
+            string name = temp["name"].str;
+            string description = temp["description"].str;
+            int capacity = (int)temp["capacity"].n;
+            int buyPrice = (int)temp["buyPrice"].n;
+            int sellPrice = (int)temp["sellPrice"].n;
+            string sprite = temp["sprite"].str;
+            Item.ItemQuality quality = (Item.ItemQuality)System.Enum.Parse(typeof(Item.ItemQuality), temp["quality"].str);
+            Item.ItemType type = (Item.ItemType)System.Enum.Parse(typeof(Item.ItemType), temp["type"].str);
+
+            Item item = null;
+            switch (type)
+            {
+                case Item.ItemType.Consumable:
+                    int hp = (int)temp["hp"].n;
+                    int mp = (int)temp["mp"].n;
+                    item = new Consumable(id, name, type, quality, description, capacity, buyPrice, sellPrice, sprite, hp, mp);
+                    break;
+                case Item.ItemType.Equipment:
+                    // todo
+                    break;
+                case Item.ItemType.Weapon:
+                    // todo
+                    break;
+                case Item.ItemType.Material:
+                    // todo
+                    break;
+                default:
+                    break;
+            }
+
+            Debug.Log("item.id = " + item.ID + " , consumable.hp = " + ((Consumable)item).HP);
+            itemList.Add(item);
+        }
     }
 
 }
